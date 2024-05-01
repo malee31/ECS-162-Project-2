a0d8a3859b4389c8b6379f0667c27f52e3f571fc
Marvin Lee <marvinlee.github@gmail.com>
2024-04-30 22:39:21 -0700

    Really bad typing game that hates Shift, Ctrl, Backspace, and Caps Lock

--- TypingTyper/js/script.js
+++ TypingTyper/js/script.js
@@ -2,17 +2,32 @@
 // document.getElementById("test11").innerHTML = timeLeft;
 
 const typedText = document.querySelector(".type-container p");
+const characters = [];
+let typeIndex = 0;
 
 function loadText() {
-    typedText.innerHTML = "";
+	typedText.innerHTML = "";
 
-    textToType[0].split('').forEach(char => {
-        let span = `<span>${char}</span>`
-        typedText.innerHTML += span;
-    });
+	textToType[0].split('').forEach(char => {
+		let span = `<span>${char}</span>`
+		typedText.innerHTML += span;
+	});
+
+	typedText.childNodes.forEach(span => {
+		characters.push(span);
+	})
+
+	document.getElementById("type-area").addEventListener("keydown", e => {
+		const keyPressed = e.key;
+
+		if(keyPressed === characters[typeIndex].innerText) {
+			characters[typeIndex].style = "color: green;";
+		} else {
+			characters[typeIndex].style = "color: red;";
+		}
+
+		typeIndex++;
+	});
 }
 
 loadText();
-// window.onload = function() {
-//     typedText.innerHTML = textToType[0];
-// }
